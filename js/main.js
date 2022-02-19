027cd6a968c84c6216d93faf07e4f0db9dfd51ef
Alex <koihoz@mail.ru>
2022-02-19 23:12:38 +0100

    исправляет ошибки

--- js/main.js
+++ js/main.js
@@ -1,25 +1,37 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно.
-const getRandomIntInclusive = (min,max) => {
+const getRandomIntInclusive = (min, max) => {
+  if (min < 0 || max < 0) {
+    return 'Введите число равное или больше нуля';
+  }
+  if (max < min) {
+    const x = max;
+    max = min;
+    min = x;
+  }
+  if (max === min) {
+    return max;
+  }
+
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 const getRandomNumber = (min, max, decimal) => {
-  if (max > min && min >= 0) {
-    return Number((Math.random() * (max - min) + min).toFixed(decimal));
+  if (min < 0 || max < 0) {
+    return 'Введите число равное или больше нуля';
   }
-  if (max < min && min >= 0){
+  if (max === min) {
+    return max;
+  }
+  if (max < min && min >= 0) {
     const x = max;
     max = min;
     min = x;
-    return Number((Math.random() * (max - min) + min).toFixed(decimal));
   }
-  if(max === min && min >= 0){
-    return max;
-  }
+  return Number((Math.random() * (max - min) + min).toFixed(decimal));
 };
 
 
