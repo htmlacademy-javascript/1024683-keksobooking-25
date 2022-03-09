e9ea589ac7494af78d31124ed2bc2b5bad2973a4
Alex <koihoz@mail.ru>
2022-03-09 22:53:47 +0100

    добавляет генерацию фото и опций

--- js/generation.js
+++ js/generation.js
@@ -20,6 +20,30 @@ const typeProperty ={
   palace: 'Дворец',
   hotel: 'Отель'};
 
+const createPhotos = (photosList) => {
+  const photosContainer = document.createDocumentFragment();
+  photosList.forEach((photo) => {
+    const photoElement = document.createElement('img');
+    photoElement.src = photo;
+    photoElement.classList.add('popup__photo');
+    photoElement.width = 45;
+    photoElement.height = 40;
+    photoElement.alt = 'Фотография жилья';
+    photosContainer.appendChild(photoElement);
+  });
+  return photosContainer;
+};
+
+const createFeatures = (featuresList) => {
+  const featuresContainer = document.createDocumentFragment();
+  featuresList.forEach((feature) => {
+    const featureElement = document.createElement('li');
+    featureElement.classList.add('popup__feature', `popup__feature--${feature}`);
+    featuresContainer.appendChild(featureElement);
+  });
+  return featuresContainer;
+};
+
 const oneCard = (card) => {
   cardElement.querySelector('.popup__title').textContent = card.offer.title;
   cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
@@ -30,9 +54,15 @@ const oneCard = (card) => {
   cardElement.querySelector('.popup__text--time').textContent = `Заезд после${card.offer.checkin}, выезд до ${card.offer.checkout}`;
   cardElement.querySelector('.popup__features').textContent = card.offer.features;
   cardElement.querySelector('.popup__description').textContent = card.offer.description;
-  cardElement.querySelector('.popup__photos').innerHTML = card.offer.photos;
   cardElement.querySelector('.popup__avatar').src = card.author.avatar;
-
+  //
+  const photos = createPhotos(card.offer.photos);
+  cardElement.querySelector('.popup__photos').innerHTML = '';
+  cardElement.querySelector('.popup__photos').appendChild(photos);
+  //
+  const features = createFeatures(card.offer.features);
+  cardElement.querySelector('.popup__features').innerHTML = '';
+  cardElement.querySelector('.popup__features').appendChild(features);
 };
 oneCard(similarCards[0]);
 
