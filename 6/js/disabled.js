01786d433001a479032678780584f5790176119d
Keks <keks@htmlacademy.ru>
2022-03-10 19:55:12 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/disabled.js
@@ -0,0 +1,37 @@
+const disabledForm = () => {
+  const adForm = document.querySelector('.ad-form');
+  adForm.classList.add('ad-form--disabled');
+  const adFormAll = adForm.children;
+  for (let i = 0; i < adFormAll.length; i++) {
+    adFormAll[i].setAttribute('disabled', '');
+  }
+
+  const mapFilters = document.querySelector('.map__filters');
+  mapFilters.classList.add('ad-form--disabled');
+  const mapFiltersAll = mapFilters.children;
+  for (let i = 0; i < mapFiltersAll.length; i++) {
+    mapFiltersAll[i].setAttribute('disabled', '');
+  }
+};
+
+disabledForm();
+
+
+const includedForm = () => {
+  const adForm = document.querySelector('.ad-form');
+  adForm.classList.remove('ad-form--disabled');
+  const adFormAll = adForm.children;
+  for (let i = 0; i < adFormAll.length; i++) {
+    adFormAll[i].removeAttribute('disabled', '');
+  }
+
+  const mapFilters = document.querySelector('.map__filters');
+  mapFilters.classList.remove('ad-form--disabled');
+  const mapFiltersAll = mapFilters.children;
+  for (let i = 0; i < mapFiltersAll.length; i++) {
+    mapFiltersAll[i].removeAttribute('disabled', '');
+  }
+};
+
+
+includedForm();
---
+++ 6/js/main.js
@@ -0,0 +1,10 @@
+import './generation.js';
+import './disabled.js';
+import {
+  createAdverts,
+  ADVERTS_QUANTITY} from './data.js';
+import {
+  renderCard} from './generation.js';
+
+const similarCards = createAdverts(ADVERTS_QUANTITY);
+renderCard(similarCards[0]);
