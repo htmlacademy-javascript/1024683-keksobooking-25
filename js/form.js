8de1e3962ecf785ea475ceca42ea47b53877accb
Alex <koihoz@mail.ru>
2022-03-13 23:57:56 +0100

    добавляет валидацию

---
+++ js/form.js
@@ -0,0 +1,13 @@
+// window.onload = function () {
+//   const form = document.getElementById('ad-form');
+//   // create the pristine instance
+//   const pristine = new Pristine(form);
+//   form.addEventListener('submit', (evt) => {
+//     evt.preventDefault();
+//     // check if the form is valid
+//     const valid = pristine.validate(); // returns true or false
+//   });
+// };
+
+//const roomNumberSelect = document.querySelector('[name="rooms"]');
+//const capacitySelect = document.querySelector('[name="capacity"]');
