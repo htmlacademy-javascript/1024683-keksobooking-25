722a8f258d8341d12538b1845a51006917b2bf97
Keks <keks@htmlacademy.ru>
2022-03-14 08:38:54 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,11 @@
+import './form.js';
+import './generation.js';
+import './disabled.js';
+import {
+  createAdverts,
+  ADVERTS_QUANTITY} from './data.js';
+import {
+  renderCard} from './generation.js';
+
+const similarCards = createAdverts(ADVERTS_QUANTITY);
+renderCard(similarCards[0]);
