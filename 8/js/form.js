db459289781320a7c589a8289a2040baaebb5608
Keks <keks@htmlacademy.ru>
2022-03-17 08:48:51 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/form.js
@@ -0,0 +1,98 @@
+const form = document.querySelector('.ad-form');
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element', // Элемент, на который будут добавляться классы
+  errorClass: 'ad-form__element--invalid', // Класс, обозначающий невалидное поле
+  successClass: 'ad-form__element--valid', // Класс, обозначающий валидное поле
+  errorTextParent: 'ad-form__element', // Элемент, куда будет выводиться текст с ошибкой
+  errorTextTag: 'fieldset', // Тег, который будет обрамлять текст ошибки
+  errorTextClass: 'ad-form__error' // Класс для элемента с текстом ошибки
+});
+const roomNumberSelect = document.querySelector('[name="rooms"]');
+const capacitySelect = document.querySelector('[name="capacity"]');
+const roomCapacityMap = {
+  '1': ['1'],
+  '2':['1', '2'],
+  '3':['1', '2', '3'],
+  '100':['0'],
+};
+
+function validateAmount () {
+  //const capacitySelectValue = String(capacitySelect.value);
+  return roomCapacityMap[roomNumberSelect.value].includes(capacitySelect.value);
+}
+
+
+function getAmountErrorMessage () {
+  let message = 'Количество гостей не может превышать количество комнат';
+  if (roomNumberSelect.value === '100'){
+    message = '100 комнат не для гостей';}
+  return message;
+}
+
+pristine.addValidator(
+  roomNumberSelect,
+  validateAmount,
+  getAmountErrorMessage
+);
+
+pristine.addValidator(
+  capacitySelect,
+  validateAmount,
+  getAmountErrorMessage
+);
+
+form.addEventListener('submit', (evt) => {
+  const value = pristine.validate();
+  if(!value){evt.preventDefault();}
+});
+
+const selectTimeIn = document.querySelector('[name="timein"]');
+const selectTimeOut = document.querySelector('[name="timeout"]');
+
+//при изменении времени выезда, меняем время въезда
+const setTimeIn = (evt) => {
+  selectTimeIn.value = evt.target.value;
+};
+//при изменении времени въезда, меняем время выезда
+const setTimeOut = (evt) => {
+  selectTimeOut.value = evt.target.value;
+};
+
+//Вешаем обработчик события change на изменение времени въезда и выезда
+selectTimeOut.addEventListener('change', setTimeIn);
+selectTimeIn.addEventListener('change', setTimeOut);
+
+
+const selectTypeHousing = document.querySelector('[name="type"]');
+const inputPrice = document.querySelector('[name="price"]');
+
+//Создаем объект cоответствия типа размещения к минимальной стоимости
+const propertyMinPrice ={
+  flat: 1000,
+  bungalow: 0,
+  house: 5000,
+  palace: 10000,
+  hotel: 3000
+};
+//при изменении типа жилья, меняем placeholder и min в инпуте стоимости жилья
+const setPrice = (evt) => {
+  inputPrice.placeholder = propertyMinPrice[evt.target.value];
+  //inputPrice.min = propertyMinPrice[evt.target.value];
+};
+
+//Вешаем обработчик события change на изменение типа жилья
+selectTypeHousing.addEventListener('change', setPrice);
+
+//Cравниваем мин значение стоимости жилья и введеную стиомость
+const validatePrice = (value) => propertyMinPrice[selectTypeHousing.value] <= Number(value);
+//Выводим сообщение об ошибке
+const getPriceErrorMessage = ()=>`Minimum value for this field is ${propertyMinPrice[selectTypeHousing.value]}`;
+
+//Валидируем инпут стоимости жилья
+pristine.addValidator(
+  inputPrice,
+  validatePrice,
+  getPriceErrorMessage,
+  1,
+  true
+);
---
+++ 8/js/generation.js
@@ -0,0 +1,65 @@
+// Находим фрагмент с содержимым темплейта
+const similarCardTemplate = document.querySelector('#card')
+  .content
+  .querySelector('.popup');
+
+//Клонируем шаблон
+const cardElement = similarCardTemplate.cloneNode(true);
+const typeProperty ={
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель'};
+
+const renderPhotos = (photosList) => {
+  const photosContainer = document.createDocumentFragment();
+  photosList.forEach((photo) => {
+    const photoElement = document.createElement('img');
+    photoElement.src = photo;
+    photoElement.classList.add('popup__photo');
+    photoElement.width = 45;
+    photoElement.height = 40;
+    photoElement.alt = 'Фотография жилья';
+    photosContainer.appendChild(photoElement);
+  });
+  return photosContainer;
+};
+
+const renderFeatures = (featuresList) => {
+  const featuresContainer = document.createDocumentFragment();
+  featuresList.forEach((feature) => {
+    const featureElement = document.createElement('li');
+    featureElement.classList.add('popup__feature', `popup__feature--${feature}`);
+    featuresContainer.appendChild(featureElement);
+  });
+  return featuresContainer;
+};
+
+const renderCard = (card) => {
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = `${card.offer.price} ₽/ночь`;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__type').textContent = typeProperty[card.offer.type];
+  cardElement.querySelector('.popup__text--capacity').textContent = `${card.offer.rooms} комнаты для ${card.offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после${card.offer.checkin}, выезд до ${card.offer.checkout}`;
+  cardElement.querySelector('.popup__features').textContent = card.offer.features;
+  cardElement.querySelector('.popup__description').textContent = card.offer.description;
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+  //
+  const photos = renderPhotos(card.offer.photos);
+  cardElement.querySelector('.popup__photos').innerHTML = '';
+  cardElement.querySelector('.popup__photos').appendChild(photos);
+  //
+  const features = renderFeatures(card.offer.features);
+  cardElement.querySelector('.popup__features').innerHTML = '';
+  cardElement.querySelector('.popup__features').appendChild(features);
+};
+
+//Находим временное расположение объявления и отрисовываем шаблон там
+const similarListElement = document.querySelector('.map__canvas');
+similarListElement.appendChild(cardElement);
+
+export {
+  renderCard};
